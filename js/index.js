41e7dbc420e1fcf8212dcc39a8781a94ab7db775
Felicien <Felicien@DESKTOP-FTUA04H>
2015-09-30 21:22:28 +0200

    Improved hash management

--- js/index.js
+++ js/index.js
@@ -1,21 +1,35 @@
 /************ smooth scroll ************/
 $('a[href*=#]:not([href=#])').click(function() {
-	goToElement(this.hash.slice(1));
+	goToElement(this.hash);
 });
 
-function goToElement(elementName) {
-	var $element = $("#"+elementName);
-	if ($element.length === 0) {
-		$element = $("[name="+elementName+"]");
-	}
-	if ($element.length > 0) {
-		$element.addClass("highlight");
-		$('html, body').animate({
-			scrollTop: $element.offset().top - Math.max(100, 0.2 * $(window).height())
+if (window.location.hash) {
+	goToElement(window.location.hash);
+}
+$(window).on('hashchange', function() {
+	goToElement(window.location.hash);
+});
+
+function goToElement(hash) {
+	if (hash && hash !== "#") {
+		var elementName = hash.replace(/^#/, '');
+		var $element = $("#"+elementName);
+		if ($element.length === 0) {
+			$element = $("[name="+elementName+"]");
+		}
+		if ($element.length > 0 && !$element.hasClass("highlight")) {
+			$element.addClass("highlight");
+			$('html, body').stop().animate({
+				scrollTop: $element.offset().top - Math.max(100, 0.2 * $(window).height())
+			}, 250);
+			setTimeout(function() {
+				$element.removeClass("highlight");
+			}, 500);
+		}
+	} else {
+		$('html, body').stop().animate({
+			scrollTop: 0
 		}, 250);
-		setTimeout(function() {
-			$element.removeClass("highlight");
-		}, 500);
 	}
 }
 
