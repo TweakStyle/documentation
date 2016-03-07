6db1359b5391942cea49e8bea72de5f0e8743074
Félicien François <felicien@tweakstyle.com>
2016-03-07 17:44:30 +0100

    analytics links update

--- js/analytics.js
+++ js/analytics.js
@@ -3,11 +3,11 @@ var _paq = _paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function() {
-  var u="//piwik.tweakstyle.com/";
-  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  var u="//stats.tweakstyle.com/";
+  _paq.push(['setTrackerUrl', u+'a.php']);
   _paq.push(['setSiteId', 1]);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'a.js'; s.parentNode.insertBefore(g,s);
 })();
 
 // universal analytics
