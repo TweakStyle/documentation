e9e6bb71204720df23cf54031e26eac0ddf6e1e5
Félicien François <felicien@tweakstyle.com>
2016-03-07 12:32:08 +0100

    switch to piwik analytics

--- js/analytics.js
+++ js/analytics.js
@@ -1,3 +1,15 @@
+// piwik
+var _paq = _paq || [];
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+  var u="//piwik.tweakstyle.com/";
+  _paq.push(['setTrackerUrl', u+'piwik.php']);
+  _paq.push(['setSiteId', 1]);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+})();
+
 // universal analytics
 (function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -11,6 +23,8 @@ window.onerror = function(errorMsg, url, line) {
 		// don't report errors in ga script
 		return;
 	}
+	// send to piwik
+	_paq.push(["trackEvent", "error", "window.onerror", url + ":" + line + " - "+errorMsg]);
 	// send to ga error reporting
 	ga("send", {
 		"hitType" : "event",
